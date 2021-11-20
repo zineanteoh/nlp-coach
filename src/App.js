af10cf718326ba773bb6be1a674cb86fbaf1efa5
Zi Nean Teoh <zinean00@gmail.com>
2021-11-19 22:42:17 -0600

    added simple frontend design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Home } from "./pages/Home";
+import "./App.css";
+
 function App() {
 	return (
-		<div className="App">
+		<div className="app">
 			<h1>Hello world</h1>
+			<Home />
 		</div>
 	);
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Home.css";
+
+export const Home = () => {
+	return (
+		<div className="container">
+			<div>Speak to me!</div>
+		</div>
+	);
+};
