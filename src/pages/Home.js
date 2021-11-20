d4c387864a6b50d8c15e0e3d88a643c41ea1a322
Zi Nean Teoh <zinean00@gmail.com>
2021-11-20 11:33:49 -0600

    removed SpeechWave.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./Home.css";
-import { SpeechWave } from "./SpeechWave";
 import { ReactSpeechRecognition } from "./ReactSpeechRecognition";
 
 export const Home = () => {
 	return (
 		<div className="container">
-			{/* <SpeechWave /> */}
 			<ReactSpeechRecognition />
 		</div>
 	);
