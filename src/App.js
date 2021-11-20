7377da709a81378b2caf45b995a9e7758bd39afa
Zi Nean Teoh <zinean00@gmail.com>
2021-11-20 11:07:15 -0600

    implemented react-speech-recognition and speechly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Home } from "./pages/Home";
 import "./App.css";
+import logo from "./assets/bot.png";
 
 function App() {
 	return (
 		<div className="app">
+			<img alt="bot" src={logo} className="botImg"></img>
 			<h1>speech app demo</h1>
 			<Home />
 		</div>
--- src/pages/Microphone2.js
+++ src/pages/Microphone2.js
@@ -1,47 +1,35 @@
 import React from "react";
+import { createSpeechlySpeechRecognition } from "@speechly/speech-recognition-polyfill";
+import SpeechRecognition, {
+	useSpeechRecognition,
+} from "react-speech-recognition";
+
+const appId = "90de1abe-ef06-4414-8d4a-53c3505eb489";
+const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
+SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
 
 export const Microphone2 = () => {
-	const audioContext = new AudioContext();
+	const { transcript, listening, browserSupportsSpeechRecognition } =
+		useSpeechRecognition();
+	const startListening = () =>
+		SpeechRecognition.startListening({ continuous: true });
 
-	// create buffer
-	const TIME_IN_SECONDS = 1;
-	const buffer = audioContext.createBuffer(
-		1,
-		audioContext.sampleRate * TIME_IN_SECONDS,
-		audioContext.sampleRate
-	);
-
-	// read the buffer of index 0
-	const channelData = buffer.getChannelData(0);
-
-	for (let i = 0; i < buffer.length; ++i) {
-		channelData[i] = Math.random() * 2 - 1;
+	if (!browserSupportsSpeechRecognition) {
+		return <span>Browser doesn't support speech recognition.</span>;
 	}
-
-	// turn volume down by creating a gainNode
-	const primaryGainControl = audioContext.createGain();
-	primaryGainControl.gain.setValueAtTime(0.02, 0);
-
-	// connect audio node to destination audio node (to play audio on user computer)
-	primaryGainControl.connect(audioContext.destination);
-
-	// call start on source node to play audio
-
-	console.log(channelData.length);
-
-	const playWhiteNoise = () => {
-		// create bufferSource that plays the audio in buffer
-		const whiteNoiseSource = audioContext.createBufferSource();
-		whiteNoiseSource.buffer = buffer;
-		whiteNoiseSource.connect(primaryGainControl);
-		whiteNoiseSource.start();
-	};
 
 	return (
 		<div>
-			<button onClick={playWhiteNoise}>
-				Click Me To Play White Noise
+			<p>Microphone: {listening ? "on" : "off"}</p>
+			<button
+				onTouchStart={startListening}
+				onMouseDown={startListening}
+				onTouchEnd={SpeechRecognition.stopListening}
+				onMouseUp={SpeechRecognition.stopListening}
+			>
+				Hold to talk
 			</button>
+			<p>{transcript}</p>
 		</div>
 	);
 };
